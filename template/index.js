6fd6b1d49239df362d168689674ecec76f717c23
Nguyen Nhu Son <nhuson269@outlook.com>
2022-06-13 22:13:20 +0700

    fix bug import App

--- template/index.js
+++ template/index.js
@@ -7,7 +7,7 @@ import React from "react";
 import messaging from "@react-native-firebase/messaging";
 import { name as appName } from "./app.json";
 import { AppRegistry } from "react-native";
-import App from "./src/app.tsx";
+import App from "./src/App";
 
 messaging().setBackgroundMessageHandler(async _remoteMessage => {
   // console.log('Message handled in the background!', remoteMessage);
