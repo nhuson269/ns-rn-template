17f272ccea59396991056442a72ecd6cb5583f4f
Nguyen Nhu Son <nhuson269@outlook.com>
2022-06-15 17:04:57 +0700

    Add color-storage

--- template/src/stores/ColorStore.ts
+++ template/src/stores/ColorStore.ts
@@ -1,4 +1,6 @@
+import { AsyncStorage } from "utils/storage-utils";
 import create from "zustand";
+import { persist } from "zustand/middleware";
 
 type ColorStore = {
   // text
@@ -21,24 +23,32 @@ type ColorStore = {
   setViolet: () => void;
 };
 
-export const colorStore = create<ColorStore>(set => ({
-  t_01: "#FFFFFF",
-  t_02: "#1F1C39",
-  t_03: "green",
-  bg_01: "#FFFFFF",
-  bg_02: "#F0F5FA",
-  b_01: "#C9D9E0",
-  b_02: "#F4F6F8",
-  line_01: "#F1E0DC",
-  unactive_02: "#BBC3C6",
-  error: "red",
-  setGreen: () => {
-    set({ t_03: "green" });
-  },
-  setBlue: () => {
-    set({ t_03: "blue" });
-  },
-  setViolet: () => {
-    set({ t_03: "violet" });
-  },
-}));
+export const colorStore = create<ColorStore, [["zustand/persist", ColorStore]]>(
+  persist(
+    set => ({
+      t_01: "#FFFFFF",
+      t_02: "#1F1C39",
+      t_03: "green",
+      bg_01: "#FFFFFF",
+      bg_02: "#F0F5FA",
+      b_01: "#C9D9E0",
+      b_02: "#F4F6F8",
+      line_01: "#F1E0DC",
+      unactive_02: "#BBC3C6",
+      error: "red",
+      setGreen: () => {
+        set({ t_03: "green" });
+      },
+      setBlue: () => {
+        set({ t_03: "blue" });
+      },
+      setViolet: () => {
+        set({ t_03: "violet" });
+      },
+    }),
+    {
+      name: "color-storage",
+      getStorage: () => AsyncStorage,
+    },
+  ),
+);
--- template/src/utils/storage-utils.ts
+++ template/src/utils/storage-utils.ts
@@ -1,11 +1,35 @@
 import { MMKV } from "react-native-mmkv";
 
+// interface AsyncStorage {
+//   getItem: (key: string) => string | null | Promise<string | null>;
+//   setItem: (key: string, value: string) => void | Promise<void>;
+//   removeItem: (key: string) => void | Promise<void>;
+// }
+
+const storage = new MMKV();
+
+function getItem(key: string) {
+  return storage.getString(key) as string | null | Promise<string | null>;
+}
+
+function setItem(key: string, value: string) {
+  return storage.set(key, value) as void | Promise<void>;
+}
+
+function removeItem(key: string) {
+  return storage.delete(key) as void | Promise<void>;
+}
+
+export const AsyncStorage = {
+  getItem,
+  setItem,
+  removeItem,
+};
+
 export enum StorageKey {
   LANGUAGE = "language", // value is string
   NOTIFY_TOKEN = "notification_token", // value is sting
   USER_PROFILE = "user_profile", // value is object
 }
-
-const storage = new MMKV();
 
 export default storage;
